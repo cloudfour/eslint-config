0541a9cc09758bd2fb3d1e0ebde3a48bcddc59bd
Erik Jung <erikdavidjung@gmail.com>
2016-05-06 20:09:31 -0700

    Add initial files

---
+++ babel.js
@@ -0,0 +1,9 @@
+/**
+ * This configuration is for JavaScript that needs to be transpiled by Babel.
+ */
+module.exports = {
+  extends: './index',
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+};
