f5664a781888082701f64f2243872edd6b9698a9
Caleb Eby <caleb.eby01@gmail.com>
2020-03-26 16:35:34 -0700

    Add load-repo script

---
+++ fixtures/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    // These rules are disabled because they are very likely to trigger
+    // Because the linting files are taken out of context
+    '@cloudfour/node/no-extraneous-require': 'off',
+    '@cloudfour/node/no-missing-require': 'off',
+    '@cloudfour/node/no-missing-import': 'off',
+    '@cloudfour/node/no-extraneous-import': 'off',
+  },
+};
