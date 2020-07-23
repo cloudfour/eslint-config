ac6d8c22d1450ae30851f6a87ce0346572be2c2c
Caleb Eby <caleb.eby01@gmail.com>
2020-07-23 16:04:36 -0700

    Add got repo [broken]

---
+++ fixtures/got/source/as-promise/types.ts
@@ -0,0 +1,129 @@
+import { CancelError } from 'p-cancelable';
+import {
+  NormalizedOptions,
+  Options,
+  Response,
+  Method,
+  TimeoutError,
+  RequestError,
+  RequestEvents,
+} from '../core';
+import PCancelable = require('p-cancelable');
+
+export type ResponseType = 'json' | 'buffer' | 'text';
+
+export interface RetryObject {
+  attemptCount: number;
+  retryOptions: RequiredRetryOptions;
+  error: TimeoutError | RequestError;
+  computedValue: number;
+}
+
+export type RetryFunction = (
+  retryObject: RetryObject
+) => number | Promise<number>;
+
+export interface RequiredRetryOptions {
+  limit: number;
+  methods: Method[];
+  statusCodes: number[];
+  errorCodes: string[];
+  calculateDelay: RetryFunction;
+  maxRetryAfter?: number;
+}
+
+export interface PaginationOptions<T, R> {
+  pagination?: {
+    transform?: (response: Response<R>) => Promise<T[]> | T[];
+    filter?: (item: T, allItems: T[], currentItems: T[]) => boolean;
+    paginate?: (
+      response: Response<R>,
+      allItems: T[],
+      currentItems: T[]
+    ) => Options | false;
+    shouldContinue?: (item: T, allItems: T[], currentItems: T[]) => boolean;
+    countLimit?: number;
+    backoff?: number;
+    requestLimit?: number;
+    stackAllItems?: boolean;
+  };
+}
+
+export type BeforeRetryHook = (
+  options: NormalizedOptions,
+  error?: RequestError,
+  retryCount?: number
+) => void | Promise<void>;
+export type AfterResponseHook = (
+  response: Response,
+  retryWithMergedOptions: (options: Options) => CancelableRequest<Response>
+) =>
+  | Response
+  | CancelableRequest<Response>
+  | Promise<Response | CancelableRequest<Response>>;
+
+// These should be merged into Options in core/index.ts
+export namespace PromiseOnly {
+  export interface Hooks {
+    beforeRetry?: BeforeRetryHook[];
+    afterResponse?: AfterResponseHook[];
+  }
+
+  export interface Options extends PaginationOptions<unknown, unknown> {
+    responseType?: ResponseType;
+    resolveBodyOnly?: boolean;
+    retry?: Partial<RequiredRetryOptions> | number;
+    isStream?: boolean;
+    encoding?: BufferEncoding;
+  }
+
+  export interface NormalizedOptions {
+    responseType: ResponseType;
+    resolveBodyOnly: boolean;
+    retry: RequiredRetryOptions;
+    isStream: boolean;
+    encoding?: BufferEncoding;
+    pagination?: Required<PaginationOptions<unknown, unknown>['pagination']>;
+  }
+
+  export interface Defaults {
+    responseType: ResponseType;
+    resolveBodyOnly: boolean;
+    retry: RequiredRetryOptions;
+    isStream: boolean;
+    pagination?: Required<PaginationOptions<unknown, unknown>['pagination']>;
+  }
+
+  export type HookEvent = 'beforeRetry' | 'afterResponse';
+}
+
+export class ParseError extends RequestError {
+  declare readonly response: Response;
+
+  constructor(error: Error, response: Response) {
+    const { options } = response.request;
+
+    super(
+      `${error.message} in "${options.url.toString()}"`,
+      error,
+      response.request
+    );
+    this.name = 'ParseError';
+
+    Object.defineProperty(this, 'response', {
+      enumerable: false,
+      value: response,
+    });
+  }
+}
+
+export interface CancelableRequest<
+  T extends Response | Response['body'] = Response['body']
+> extends PCancelable<T>, RequestEvents<CancelableRequest<T>> {
+  json: <ReturnType>() => CancelableRequest<ReturnType>;
+  buffer: () => CancelableRequest<Buffer>;
+  text: () => CancelableRequest<string>;
+}
+
+export { CancelError };
+export * from '../core';
---
+++ fixtures/got/source/core/utils/timed-out.ts
@@ -0,0 +1,214 @@
+import { ClientRequest, IncomingMessage } from 'http';
+import unhandler from './unhandle';
+import net = require('net');
+
+const reentry: unique symbol = Symbol('reentry');
+const noop = (): void => {};
+
+interface TimedOutOptions {
+  host?: string;
+  hostname?: string;
+  protocol?: string;
+}
+
+export interface Delays {
+  lookup?: number;
+  connect?: number;
+  secureConnect?: number;
+  socket?: number;
+  response?: number;
+  send?: number;
+  request?: number;
+}
+
+export type ErrorCode =
+  | 'ETIMEDOUT'
+  | 'ECONNRESET'
+  | 'EADDRINUSE'
+  | 'ECONNREFUSED'
+  | 'EPIPE'
+  | 'ENOTFOUND'
+  | 'ENETUNREACH'
+  | 'EAI_AGAIN';
+
+export class TimeoutError extends Error {
+  code: ErrorCode;
+
+  constructor(threshold: number, public event: string) {
+    super(`Timeout awaiting '${event}' for ${threshold}ms`);
+
+    this.name = 'TimeoutError';
+    this.code = 'ETIMEDOUT';
+  }
+}
+
+export default (
+  request: ClientRequest,
+  delays: Delays,
+  options: TimedOutOptions
+): (() => void) => {
+  if (reentry in request) {
+    return noop;
+  }
+
+  request[reentry] = true;
+  const cancelers: typeof noop[] = [];
+  const { once, unhandleAll } = unhandler();
+
+  const addTimeout = (
+    delay: number,
+    callback: (delay: number, event: string) => void,
+    event: string
+  ): typeof noop => {
+    const timeout = (setTimeout(
+      callback,
+      delay,
+      delay,
+      event
+    ) as unknown) as NodeJS.Timeout;
+
+    timeout.unref?.();
+
+    const cancel = (): void => {
+      clearTimeout(timeout);
+    };
+
+    cancelers.push(cancel);
+
+    return cancel;
+  };
+
+  const { host, hostname } = options;
+
+  const timeoutHandler = (delay: number, event: string): void => {
+    request.destroy(new TimeoutError(delay, event));
+  };
+
+  const cancelTimeouts = (): void => {
+    for (const cancel of cancelers) {
+      cancel();
+    }
+
+    unhandleAll();
+  };
+
+  request.once('error', (error) => {
+    cancelTimeouts();
+
+    // Save original behavior
+    /* istanbul ignore next */
+    if (request.listenerCount('error') === 0) {
+      throw error;
+    }
+  });
+
+  request.once('close', cancelTimeouts);
+
+  once(request, 'response', (response: IncomingMessage): void => {
+    once(response, 'end', cancelTimeouts);
+  });
+
+  if (typeof delays.request !== 'undefined') {
+    addTimeout(delays.request, timeoutHandler, 'request');
+  }
+
+  if (typeof delays.socket !== 'undefined') {
+    const socketTimeoutHandler = (): void => {
+      timeoutHandler(delays.socket!, 'socket');
+    };
+
+    request.setTimeout(delays.socket, socketTimeoutHandler);
+
+    // `request.setTimeout(0)` causes a memory leak.
+    // We can just remove the listener and forget about the timer - it's unreffed.
+    // See https://github.com/sindresorhus/got/issues/690
+    cancelers.push(() => {
+      request.removeListener('timeout', socketTimeoutHandler);
+    });
+  }
+
+  once(request, 'socket', (socket: net.Socket): void => {
+    const { socketPath } = request as ClientRequest & { socketPath?: string };
+
+    /* istanbul ignore next: hard to test */
+    if (socket.connecting) {
+      const hasPath = Boolean(
+        socketPath ?? net.isIP(hostname ?? host ?? '') !== 0
+      );
+
+      if (
+        typeof delays.lookup !== 'undefined' &&
+        !hasPath &&
+        typeof (socket.address() as net.AddressInfo).address === 'undefined'
+      ) {
+        const cancelTimeout = addTimeout(
+          delays.lookup,
+          timeoutHandler,
+          'lookup'
+        );
+        once(socket, 'lookup', cancelTimeout);
+      }
+
+      if (typeof delays.connect !== 'undefined') {
+        const timeConnect = (): (() => void) =>
+          addTimeout(delays.connect!, timeoutHandler, 'connect');
+
+        if (hasPath) {
+          once(socket, 'connect', timeConnect());
+        } else {
+          once(socket, 'lookup', (error: Error): void => {
+            if (error === null) {
+              once(socket, 'connect', timeConnect());
+            }
+          });
+        }
+      }
+
+      if (
+        typeof delays.secureConnect !== 'undefined' &&
+        options.protocol === 'https:'
+      ) {
+        once(socket, 'connect', (): void => {
+          const cancelTimeout = addTimeout(
+            delays.secureConnect!,
+            timeoutHandler,
+            'secureConnect'
+          );
+          once(socket, 'secureConnect', cancelTimeout);
+        });
+      }
+    }
+
+    if (typeof delays.send !== 'undefined') {
+      const timeRequest = (): (() => void) =>
+        addTimeout(delays.send!, timeoutHandler, 'send');
+      /* istanbul ignore next: hard to test */
+      if (socket.connecting) {
+        once(socket, 'connect', (): void => {
+          once(request, 'upload-complete', timeRequest());
+        });
+      } else {
+        once(request, 'upload-complete', timeRequest());
+      }
+    }
+  });
+
+  if (typeof delays.response !== 'undefined') {
+    once(request, 'upload-complete', (): void => {
+      const cancelTimeout = addTimeout(
+        delays.response!,
+        timeoutHandler,
+        'response'
+      );
+      once(request, 'response', cancelTimeout);
+    });
+  }
+
+  return cancelTimeouts;
+};
+
+declare module 'http' {
+  interface ClientRequest {
+    [reentry]: boolean;
+  }
+}
