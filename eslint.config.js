cdeaa872657ff58100a22e04f2c51e85dacfe751
Scott Vandehey <scott@cloudfour.com>
2024-11-12 14:06:26 -0800

    add globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,24 @@
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
 	js.configs.recommended,
 
 	{
+		languageOptions: {
+			ecmaVersion: 2022,
+			sourceType: 'module',
+			parserOptions: {
+				ecmaFeatures: { jsx: true },
+			},
+			globals: {
+				...globals.es2021,
+				...globals.node,
+				document: false,
+				navigator: false,
+				window: false,
+			},
+		},
 		rules: {
 			'no-unused-vars': 'warn',
 			'no-undef': 'warn',
